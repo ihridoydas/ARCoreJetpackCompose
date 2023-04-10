a76df9c1af83ff05538c76198606599923ca5238
Hridoy Das <hridoy@qualias.jp>
2023-04-10 23:14:56 +0900

    AR Core Jetpack compose and xml

---
+++ app/src/main/java/com/ihridoydas/arcorejetpackcompose/Constants.kt
@@ -0,0 +1,8 @@
+package com.ihridoydas.arcorejetpackcompose
+
+object Constants{
+    const val maxNumMultiplePoints = 6
+    const val multipleDistanceTableHeight = 300
+
+    const val arrowViewSize = 45
+}
